edda50d3a73258ed5330f4fe1b4b0926468f4818
keykey <keyyew@gmail.com>
2022-11-29 18:16:22 +0800

    added script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let color = "black";
+let click = true;
+
 function populateBoard(size) {
   let board = document.querySelector(".board");
   let squares = board.querySelectorAll("div");
@@ -17,12 +20,41 @@ populateBoard(16);
 
 function changeSize(input) {
   if (input >= 2 && input <= 100) {
+    document.querySelector(".error").style.display = "none";
     populateBoard(input);
   } else {
+    document.querySelector(".error").style.display = "flex";
     console.log("invalid input");
   }
 }
 
 function colorSquare() {
-  this.style.backgroundColor = "black";
+  if (click) {
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
 }
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+  if (e.target.tagName != "BUTTON") {
+    click = !click;
+    if (click) {
+      document.querySelector(".mode").textContent = "Mode: coloring";
+    } else {
+      document.querySelector(".mode").textContent = "Mode: Not coloring";
+    }
+  }
+});
