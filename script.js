14d291f3501878e98b3729cc74694d70f997712c
keykey <keyyew@gmail.com>
2022-11-29 17:35:39 +0800

    added board

---
+++ script.js
@@ -0,0 +1,28 @@
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("invalid input");
+  }
+}
+
+function colorSquare() {
+  this.style.backgroundColor = "black";
+}
